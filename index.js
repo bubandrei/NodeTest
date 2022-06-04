b8edffa84179003b8e91df76a65d37186e5081fc
a.bubelev <bubeleu@gmail.com>
2022-06-04 18:11:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const start = () => {
+    try {
+
+    } catch (e) {
+        console.log(e);
+    }
+}
