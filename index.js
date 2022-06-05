d8241718a8d890bb7f91c629fe5221588247a1fa
a.bubelev <bubeleu@gmail.com>
2022-06-05 15:01:47 +0300

    error create db

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 import express from "express";
-const PORT = process.env.PORT || 3000;
+import mysql from 'mysql';
+
+const PORT = 3000;
+
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Users'
+
+});
+mysqlConnection.connect((err) => {
+    if (err) {
+        console.log('ERROR');
+    } else {
+        console.log('DataBase sacceded');
+    }
+});
 
 const app = express();
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     console.log(req.query);
+//     res.status(200).json("Server is working");
+// })
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).json("Server is working");
+})
 
 const start = () => {
     try {
